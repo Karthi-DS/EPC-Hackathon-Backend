83d3038be48c21c40c2f3a752f0ac4b90eacb9fe
dheepthiramakrishnan <dheepthi.ramu@gmail.com>
2026-04-11 17:26:57 +0530

    update routes

--- app.js
+++ app.js
@@ -3,18 +3,28 @@ require("dotenv").config();
 const express = require("express");
 const { db, testDB } = require("./config/db");
 
+const courseRoutes = require("./routes/courseRoutes");
+
 testDB();
 
 const server = express();
 
-server.get("/api/",(req,res)=>{
-  res.send("test service is running");
-})
+
+server.use(express.json());
+
+// Root route
+// server.get("/", (req, res) => {
+//   res.send("Course Service running 🚀");
+// });
+
+server.use("/api", courseRoutes);
 
 server.get("/api/courses/healthCheck", (req, res) => {
   res.send("service is good..");
 });
 
-server.listen(process.env.PORT,()=>{
-  console.log(`server is running ${process.env.PORT}`)
-})
+server.use("/", courseRoutes);
+
+server.listen(process.env.PORT, () => {
+  console.log(`server is running ${process.env.PORT}`);
+});
